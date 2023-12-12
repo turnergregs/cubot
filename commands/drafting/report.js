19f8db306af798ea7c126979bf5095df3f996d57
turnergregs <tbg435@utexas.edu>
2023-12-11 23:02:00 -0600

    feel like I'm getting closer

--- commands/drafting/report.js
+++ commands/drafting/report.js
@@ -8,16 +8,17 @@ module.exports = {
 	async execute(interaction) {
 
 		const findCubeThread = async function(cube_id){
-			let cubeListsChannel = interaction.guild.channels.cache.find(channel => channel.name === 'austin-cube-lists');
-			// console.log(cubeListsChannel.threads.cache);
+			const cubeListsChannel = interaction.guild.channels.cache.find(channel => channel.name === 'austin-cube-lists');
 			const activeThreads = await cubeListsChannel.threads.fetchActive();
-			// console.log(activeThreads.threads);
 			for(thread of activeThreads.threads){
-				console.log(thread);
+				// no idea why they make you do thread[1]
+				const messages = await thread[1].messages.fetch();
+				console.log(messages);
+				for(message of messages){
+					//why are these all empty strings??
+					console.log(message.content);
+				}
 			}
-			cubeListsChannel.threads.cache.fetchActiveThreads()
-				.then(fetched => console.log(`There are ${fetched.threads.size} threads.`))
-			//return thread;
 		}
 
 		await findCubeThread('tempocube');
